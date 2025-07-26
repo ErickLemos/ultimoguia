3e9d9b517b104d143f0a920141342475d16c471d
Erick B. Lemos <ericklemoscosta@gmail.com>
2025-07-26 19:06:17 -0300

    feature: configuracao basica da aplicacao

---
+++ src/main/kotlin/com/ericklemos/ultimoguia/UltimoguiaApplication.kt
@@ -0,0 +1,11 @@
+package com.ericklemos.ultimoguia
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UltimoguiaApplication
+
+fun main(args: Array<String>) {
+	runApplication<UltimoguiaApplication>(*args)
+}
---
+++ src/test/kotlin/com/ericklemos/ultimoguia/UltimoguiaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ericklemos.ultimoguia
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UltimoguiaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
