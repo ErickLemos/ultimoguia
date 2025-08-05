1cb01278d73e2713f6def5eedf8905c57e17e2dc
Erick B. Lemos <ericklemoscosta@gmail.com>
2025-08-04 21:38:35 -0300

    refactor: remoção do modo sem CDS - teste já realizado

--- command.build-docker.sh
+++ command.build-docker.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 docker build -t ultimoguia:cds .
-cat withoutCds.Dockerfile | docker buildx build -t ultimoguia:withoutCds -f - .
-
 docker save ultimoguia:cds | sudo k3s ctr images import -
-docker save ultimoguia:withoutCds | sudo k3s ctr images import -
